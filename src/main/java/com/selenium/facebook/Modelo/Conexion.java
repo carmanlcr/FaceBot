07fd85c0cf7973c68b529556cd7c690f9774ffd3
Luis Morales <luis.andres.carman@gmail.com>
2019-12-17 10:50:43 -0500

    Update mysql driver

--- src/main/java/com/selenium/facebook/Modelo/Conexion.java
+++ src/main/java/com/selenium/facebook/Modelo/Conexion.java
@@ -6,25 +6,23 @@ import java.sql.SQLException;
 import java.sql.Connection;
 
 public class Conexion {
-	private final String URL = "jdbc:mysql://HOST:3306/"; // Ubicaci�n de la BD.
+	private final String URL = "jdbc:mysql://192.168.2.6:3306/"; // Ubicaci�n de la BD.
     private final String BD = "facebook"; // Nombre de la BD.
-    private final String USER = "username"; //Nomber del usuario
-    private final String PASSWORD = "password"; //contrase�a
+    private final String USER = "lmorales"; //Nomber del usuario
+    private final String PASSWORD = "Carabobo?18"; //contrase�a
 
     public Connection connect = null;
 
-    @SuppressWarnings("finally")
 	public Connection conectar() {
         try {
         	Class.forName("com.mysql.cj.jdbc.Driver");
-            connect = (Connection) DriverManager.getConnection(URL + BD, USER, PASSWORD);
+            connect = (Connection) DriverManager.getConnection(URL + BD+"?useSSL=false", USER, PASSWORD);
             
         }catch(SQLException e1) {
         	System.err.println("Error al conectarse a la base de datos "+e1);
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            return connect;
         }
+        return connect;
     }
 }
